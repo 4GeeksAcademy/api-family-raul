6e848ef6a3926db2b6559ecc75d66647d85bcfd3
Raul M <118123543+pillow3@users.noreply.github.com>
2024-02-07 22:49:01 +0000

    first

--- src/app.py
+++ src/app.py
@@ -25,20 +25,56 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# POST /member
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.json
+    member = request_body
+
+    jackson_family.add_member(member)
+    
+    return jsonify({"message": "Miembro agregado correctamente."}, request_body)
+
+# GET /member/id
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+
+    return jsonify({"message": "The member is"}, member)
+
+
+# DELETE /member/id
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    return({"message":"Miembro elimindado"})
+
+# GET /members
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
 
 
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+
+# solution
+#  ✗ The Family structure has to be initialized with the 3 members specified in the instructions
+#  ✗ Implement method POST /member to add a new member
+#  ✓ Method POST /member should return something, NOT EMPTY
+#  ✓ Implement method GET /members
+#  ✗ Method GET /members should return a list
+#  ✗ We added two members using POST /member, when calling the GET /members should get a list of length == 5
+#  ✗ Method GET /member/<int:id> should exist
+#  ✗ Method GET /member/<int:id> should return a one single family member in a dictionary format
+#  ✗ The dictionary returned by GET /member/<int:id> should contain one family member with the keys [name, id, age, lucky_numbers]
+#  ✗ Method GET /member/3443 should return Tommy
+#  ✗ Implement method DELETE /member/<int:id> to delete a family member
+#  ✗ Method DELETE /member/3443 should return dictionary with 'done' key
+#  ✗ After deleting the member 3443 we called GET /members and it should return a list with 4 members
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,24 +13,59 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+    # {
+    #     "name": "John Jackson",
+    #     "age": 33,
+    #     "lucky_numbers": [7, 13, 22]
+    # },
+    # {
+    #     "name": "Jane Jackson",
+    #     "age": 35,
+    #     "lucky_numbers": [10, 14, 3]
+    # },
+    # {
+    #     "name": "Jimmy Jackson",
+    #     "age": 5,
+    #     "lucky_numbers": [1]
+    # }
+]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if "id" not in member or not member["id"]:
+            member["id"] = self._generateId()
+
+            self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = [ member for member in self._members if member["id"] != id]
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        show_member = [ member for member in self._members if member["id"] == id]
+
+        return show_member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+
+
+# solution
+#  ✗ The Family structure has to be initialized with the 3 members specified in the instructions
+#  ✗ Implement method POST /member to add a new member
+#  ✓ Method POST /member should return something, NOT EMPTY
+#  ✓ Implement method GET /members
+#  ✗ Method GET /members should return a list
+#  ✗ We added two members using POST /member, when calling the GET /members should get a list of length == 5
+#  ✗ Method GET /member/<int:id> should exist
+#  ✗ Method GET /member/<int:id> should return a one single family member in a dictionary format
+#  ✗ The dictionary returned by GET /member/<int:id> should contain one family member with the keys [name, id, age, lucky_numbers]
+#  ✗ Method GET /member/3443 should return Tommy
+#  ✗ Implement method DELETE /member/<int:id> to delete a family member
+#  ✗ Method DELETE /member/3443 should return dictionary with 'done' key
+#  ✗ After deleting the member 3443 we called GET /members and it should return a list with 4 members
